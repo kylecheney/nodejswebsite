3edd7d0bcd9794b588c5c9a7b3289f93d8ab93dc
kylecheney <kylejosephcheney@gmail.com>
2016-11-14 18:35:17 -0700

    forgot to use curly bracers, instead used parenthesis like Alex me to

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ app.get('/', function(req, res) {
 app.get('/narnia', function (req,res) {
   res.send(`
     <h1>For Narnia!</h1>
-    $(data.rooms[1].name)
+    ${data.rooms[1].name}
     `)
 })
 
