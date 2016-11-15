1d78999808e62561123e479f7efd44542ade0eda
kylecheney <kylejosephcheney@gmail.com>
2016-11-15 14:43:48 -0700

    got it working

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 // var data = require('./')
 
 router.get('/', function(req, res) {
-  res.send(data.rooms[0].name);
+  res.send(`
+    <h1>It's working!</h1>
+    <img src=https://media0.giphy.com/media/9K2nFglCAQClO/200_s.gif#0></img>
+  `);
 })
 
 module.exports = router;
